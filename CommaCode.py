05dcc2eae01f6aab28819fad1f41803146bf8caf
hariprasad-reddy07 <hariprasadreddym.24.beai@acharya.ac.in>
2025-05-26 18:28:19 +0530

    Add files via upload

---
+++ CommaCode.py
@@ -0,0 +1,8 @@
+print("Hariprasad Reddy ")
+print("1AY24AI039 ")
+print(" M ")
+items = ['apples', 'bananas', 'tofu', 'cats']
+if len(items) == 0:
+    print("Empty list")
+else:
+    print(', '.join(items[:-1]) + ', and ' + items[-1])
