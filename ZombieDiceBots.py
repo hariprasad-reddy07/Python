7a0a929450790c111f13f7a2585c160b0f004c74
hariprasad-reddy07 <hariprasadreddym.24.beai@acharya.ac.in>
2025-05-26 18:54:13 +0530

    Add files via upload

---
+++ ZombieDiceBots.py
@@ -0,0 +1,7 @@
+print("Hariprasad Reddy ")
+print("1AY24AI039 ")
+print(" M ")
+# Simplified zombie dice bot
+import random
+dice = ['brain']*3 + ['shotgun']*2 + ['runner']*1
+print("Bot rolls:", ', '.join(random.sample(dice, 3)))
