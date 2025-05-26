29fc6ce413c1ae9b3467009c6595f3fe9a27997c
hariprasad-reddy07 <hariprasadreddym.24.beai@acharya.ac.in>
2025-05-26 18:34:44 +0530

    Add files via upload

---
+++ CharacterPictureGrid.py
@@ -0,0 +1,13 @@
+
+print("Hariprasad Reddy ")
+print("1AY24AI039 ")
+print(" M ")
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.']]
+for x in range(len(grid[0])):
+    for y in range(len(grid)):
+        print(grid[y][x], end='')
+    print()
+
+    
