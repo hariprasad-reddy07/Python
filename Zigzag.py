61f62722852000a6bdf749f67075592d8fbfdbec
hariprasad-reddy07 <hariprasadreddym.24.beai@acharya.ac.in>
2025-05-26 18:13:23 +0530

    Add files via upload

---
+++ Zigzag.py
@@ -0,0 +1,17 @@
+print("Hariprasad Reddy ")
+print("1AY24AI039 ")
+print(" M ")
+import time
+indent = 0
+increase = True
+while True:
+    print(' ' * indent + '********')
+    time.sleep(0.1)
+    if increase:
+        indent += 1
+        if indent == 20:
+            increase = False
+    else:
+        indent -= 1
+        if indent == 0:
+            increase = True
