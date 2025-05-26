80eaa94cd6351e46b066cce2c8d7d00bd996eee8
hariprasad-reddy07 <hariprasadreddym.24.beai@acharya.ac.in>
2025-05-26 18:37:37 +0530

    Add files via upload

---
+++ ChessDictionaryValidator.py
@@ -0,0 +1,7 @@
+print("Hariprasad Reddy ")
+print("1AY24AI039 ")
+print(" M ")
+def valid_chess_board(board):
+    # Add validation logic here
+    return True
+print(valid_chess_board({'h1': 'wking'}))  # Example usage
